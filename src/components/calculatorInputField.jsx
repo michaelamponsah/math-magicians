9382708cb6c3f4b69368c919028db8bbadf2d284
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-01-12 11:21:43 +0000

    Pass events up to parent component

--- src/components/calculatorInputField.jsx
+++ src/components/calculatorInputField.jsx
@@ -8,17 +8,28 @@ class Input extends Component {
   }
 
   render() {
-    const { btnType } = this.props;
-    return <input className="calculatorInputField" type={btnType} />;
+    const {
+      type,
+      onInputChange,
+      total,
+      next,
+    } = this.props;
+    const value = next || total;
+    return <input className="calculatorInputField" type={type} onChange={(e) => onInputChange(e)} value={value} />;
   }
 }
 
 Input.propTypes = {
-  btnType: PropTypes.string,
+  type: PropTypes.string,
+  onInputChange: PropTypes.func.isRequired,
+  total: PropTypes.string,
+  next: PropTypes.string,
 };
 
 Input.defaultProps = {
-  btnType: 'text',
+  type: 'text',
+  total: '0',
+  next: null,
 };
 
 export default Input;
