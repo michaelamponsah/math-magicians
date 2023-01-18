9cfa5917cd07e859234566f8c16ce2fcb9cab9a7
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-01-18 01:14:59 +0000

    Setup pages and navigation routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Calculator from './components/calculator';
+import Calculator from './pages/Calculator';
+import Home from './pages/Home';
+import Quotes from './pages/Quotes';
+// import Calculator from './components/calculator';
 
 const App = () => (
   <div className="App">
     <h1>Math Magicians</h1>
-    <Calculator />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quotes" element={<Quotes />} />
+      </Routes>
+    </BrowserRouter>
   </div>
 );
 
---
+++ src/pages/Calculator.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Calculator = () => (
+  <div>Calculator</div>
+);
+
+export default Calculator;
